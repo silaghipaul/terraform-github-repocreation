7b0edd84cdc9dd2e06955b9a9ccde822e870c054
Paul Silaghi <silaghipaul@yahoo.com>
2024-05-29 13:58:06 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = "~> 1.5"
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+  token ="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"
+}
+
 resource "github_repository" "repo" {
   name        = var.repo_name
   description = "my awesome codebase"
